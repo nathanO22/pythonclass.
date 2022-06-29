833b87f2b9f9b348d7fab87211dd42474b6e16a0
unknown <nathanielolowu22@gmail.com>
2022-06-29 14:59:25 -0700

    added fourthclass

---
+++ fourthclass.py/fourthclass.py
@@ -0,0 +1,18 @@
+# my = "there is a blue tree at the end of the street, it covers a large area of land."
+# def remove_chars(a:str, b:int):
+#     return a.index - b
+
+# print(remove_chars(my, 14))
+
+# def change_mine(b):
+#     b = "welcome back"
+#     x = [b, 4]
+
+#     print(x)
+# print(change_mine())
+
+my = "there is a blue tree at the end of the street, it covers a large area of land."
+def remove_chars(a:str, b:int):
+    x = a[b:]
+    return x
+print(remove_chars(my, 20))
